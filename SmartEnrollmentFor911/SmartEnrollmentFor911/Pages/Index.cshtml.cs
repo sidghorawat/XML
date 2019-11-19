97e160c3a2a60ddf9a992c4f8f78ab608b801391
medhak94 <kulkarmd@mail.uc.edu>
2019-11-18 23:33:34 -0500

    Initial Commit for Smart Enrollment

--- SmartEnrollmentFor911/SmartEnrollmentFor911/Pages/Index.cshtml.cs
+++ SmartEnrollmentFor911/SmartEnrollmentFor911/Pages/Index.cshtml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using QuickType;
+using QuickType911Enrollment;
 
 namespace SmartEnrollmentFor911.Pages
 {
@@ -15,11 +18,21 @@ namespace SmartEnrollmentFor911.Pages
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
-        }
+        } 
 
         public void OnGet()
         {
+            using (var webClient = new WebClient())
+            {
+                String crimeIncidentJSON = webClient.DownloadString("https://data.cincinnati-oh.gov/resource/k59e-2pvf.json");
+                var crimeIncidents = CrimeIncidents.FromJson(crimeIncidentJSON);
+                ViewData["CrimeIncidents"] = crimeIncidents;
 
+
+                String smart911EnrollmentJSON = webClient.DownloadString("https://data.cincinnati-oh.gov/resource/rtu7-isj6.json");
+                var smart911Enrollments = Smart911Enrollment.FromJson(smart911EnrollmentJSON);
+                ViewData["MobileFoodSchedules"] = smart911Enrollments;
+            }
         }
     }
 }
